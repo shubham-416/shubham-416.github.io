dcce7fb0935c82c85c6e853181e4de7d4161d5c4
shubham-416 <103108940+shubham-416@users.noreply.github.com>
2022-05-18 15:24:55 +0530

    Update experiments-11.py

--- experiments-11.py
+++ experiments-11.py
@@ -1,14 +1,9 @@
-side_a = int(input("Enter the first side of the triangle: "))
-
-side_b =int(input("Enter the second side of the triangle: "))
-
-side_c = int(input("Enter the third side of the triangle: "))
 (
-if(side_a == side_b == side_c):
+print("The given triangle is an equilateral triangle.")
 
-   print("The given triangle is an equilateral triangle.")
+if int(input("Enter the first side of the triangle: "))
+   ==int(input("Enter the second side of the triangle: "))
+   ==int(input("Enter the third side of the triangle: "))
 
-else:
-
-   print("The given triangle is not an equilateral triangle.")
+else  print("The given triangle is not an equilateral triangle.")
 )
