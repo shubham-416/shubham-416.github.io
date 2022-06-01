1de4909e79d771a749c3927c899f964187b85cda
shubham-416 <103108940+shubham-416@users.noreply.github.com>
2022-06-01 13:21:53 +0530

    Update and rename experiments-11.py to experiments-11.html

--- experiments-11.py
+++
@@ -1,9 +0,0 @@
-(
-print("The given triangle is an equilateral triangle.")
-
-if int(input("Enter the first side of the triangle: "))
-   ==int(input("Enter the second side of the triangle: "))
-   ==int(input("Enter the third side of the triangle: "))
-
-else  print("The given triangle is not an equilateral triangle.")
-)
